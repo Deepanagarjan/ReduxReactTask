70e2a52c255e4369142ae90cb5975b7b303bf29c
Deepanagarjan <deepanagarajan1208@gmail.com>
2024-08-23 15:37:56 +0530

    updated

--- src/Components/RootLayout.jsx
+++ src/Components/RootLayout.jsx
@@ -2,7 +2,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBarPanel";
 import { Provider } from "react-redux";
-import store from "../store/store";
+import store from "../Store/store";
 const RootLayout = () => {
   return (
     <>
